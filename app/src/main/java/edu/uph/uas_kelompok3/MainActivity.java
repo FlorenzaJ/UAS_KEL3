d09c1ceec721de8d6fe1381f37d549d358c11b39
farrelraditya <farrelradityasecond2@gmail.com>
2025-06-30 19:20:10 +0700

    Prediction Form

--- app/src/main/java/edu/uph/uas_kelompok3/MainActivity.java
+++ app/src/main/java/edu/uph/uas_kelompok3/MainActivity.java
@@ -1,15 +1,12 @@
 package edu.uph.uas_kelompok3;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import edu.uph.uas_kelompok3.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,21 +14,17 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@NonNull Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_predict, R.id.nav_profile)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        // Setup Navigation Component
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_activity_main);
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(binding.navView, navController);
+        }
     }
-
 }
--- app/src/main/java/edu/uph/uas_kelompok3/ui/predict/PredictFragment.java
+++ app/src/main/java/edu/uph/uas_kelompok3/ui/predict/PredictFragment.java
@@ -4,34 +4,76 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import edu.uph.uas_kelompok3.databinding.FragmentPredictBinding;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.slider.Slider;
+import com.google.android.material.textfield.TextInputEditText;
+
+import edu.uph.uas_kelompok3.R;
 
 public class PredictFragment extends Fragment {
 
-    private FragmentPredictBinding binding;
+    private TextInputEditText etAge;
+    private AutoCompleteTextView actvGender, actvSmokingHistory, actvCoughFrequency, actvAllergies, actvChronicConditions;
+    private Slider sliderBreathingDifficulty;
+    private MaterialButton btnGenerate;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        PredictViewModel predictViewModel =
-                new ViewModelProvider(this).get(PredictViewModel.class);
+    private String[] genderOptions = {"Male", "Female"};
+    private String[] smokingOptions = {"Never", "Former", "Current"};
+    private String[] coughOptions = {"Rarely", "Sometimes", "Often", "Always"};
+    private String[] allergyOptions = {"None", "Dust", "Pollen", "Food", "Other"};
+    private String[] chronicOptions = {"None", "Asthma", "COPD", "Diabetes", "Other"};
 
-        binding = FragmentPredictBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_predict, container, false);
 
-        final TextView textView = binding.textDashboard;
-        predictViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Initialize views
+        etAge = root.findViewById(R.id.et_age);
+        actvGender = root.findViewById(R.id.actv_gender);
+        actvSmokingHistory = root.findViewById(R.id.actv_smoking_history);
+        actvCoughFrequency = root.findViewById(R.id.actv_cough_frequency);
+        actvAllergies = root.findViewById(R.id.actv_allergies);
+        actvChronicConditions = root.findViewById(R.id.actv_chronic_conditions);
+        sliderBreathingDifficulty = root.findViewById(R.id.slider_breathing_difficulty);
+        btnGenerate = root.findViewById(R.id.btn_generate_prediction);
+
+        setupDropdown(actvGender, genderOptions);
+        setupDropdown(actvSmokingHistory, smokingOptions);
+        setupDropdown(actvCoughFrequency, coughOptions);
+        setupDropdown(actvAllergies, allergyOptions);
+        setupDropdown(actvChronicConditions, chronicOptions);
+
+        btnGenerate.setOnClickListener(v -> {
+            String age = etAge.getText().toString();
+            String gender = actvGender.getText().toString();
+            String smoking = actvSmokingHistory.getText().toString();
+            String cough = actvCoughFrequency.getText().toString();
+            String allergy = actvAllergies.getText().toString();
+            String chronic = actvChronicConditions.getText().toString();
+            float breathingDifficulty = sliderBreathingDifficulty.getValue();
+
+            String result = "Age: " + age +
+                    "\nGender: " + gender +
+                    "\nSmoking: " + smoking +
+                    "\nCough: " + cough +
+                    "\nAllergy: " + allergy +
+                    "\nChronic: " + chronic +
+                    "\nBreathing Difficulty: " + breathingDifficulty;
+
+            Toast.makeText(getContext(), "Prediction Generated!\n" + result, Toast.LENGTH_LONG).show();
+        });
+
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private void setupDropdown(AutoCompleteTextView view, String[] options) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_dropdown_item_1line, options);
+        view.setAdapter(adapter);
     }
 }
