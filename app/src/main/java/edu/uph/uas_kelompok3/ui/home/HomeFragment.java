0e8fe5113f886dcd7764f9435eac98233a9844b0
imelda-s <imeldasunario2005@gmail.com>
2025-07-15 10:30:28 +0700

    Update

--- app/src/main/java/edu/uph/uas_kelompok3/ui/home/HomeFragment.java
+++ app/src/main/java/edu/uph/uas_kelompok3/ui/home/HomeFragment.java
@@ -63,10 +63,6 @@ public class HomeFragment extends Fragment {
                 .equalTo("userId", userId)
                 .sort("createdAt", Sort.DESCENDING)
                 .findAll();
-        } else {
-            predictions = realm.where(Predict.class)
-                .sort("createdAt", Sort.DESCENDING)
-                .findAll();
         }
 
         adapter = new PredictionAdapter(predictions);
